6fdb607f0dbef28bcfd167274ca3d3f65ef4c80a
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2019-12-29 15:02:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -20,7 +20,7 @@ class Translation(object):
 
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = " **This** **is** **free** **service** **and** **there** **is** **no** **Current** **Plans** **😜**"
+    UPGRADE_TEXT = "**This** **is** **free** **service** **and** **there** **is** **no** **Current** **Plans** **😜**"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
