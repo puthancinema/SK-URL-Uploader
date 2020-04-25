2a1578ff61491bf5731396ff4a5de94ca9f763a6
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-04-26 01:10:38 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -127,3 +127,9 @@ async def rename_doc(bot, update):
                 disable_web_page_preview=True
             )
     else:
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.REPLY_TO_DOC_FOR_RENAME_FILE,
+            reply_to_message_id=update.message_id
+        )
+© 2020 GitHub, Inc.
