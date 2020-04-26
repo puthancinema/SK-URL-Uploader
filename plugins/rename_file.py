870a62b823f9a394a32f7714e358b129cd0ce676
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-04-26 20:24:07 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -48,10 +48,12 @@ async def rename_doc(bot, update):
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
             chat_id=update.chat.id,
-            text=Translation.DOWNLOAD_START,      
+            text=Translation.DOWNLOAD_START,  
+            reply_to_message_id=update.message_id
         )
         c_time = time.time()
         the_real_download_location = await bot.download_media(
+          message=update.reply_to_message,
             file_name=download_location,
             progress=progress_for_pyrogram,
             progress_args=(
