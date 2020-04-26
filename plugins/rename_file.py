e11ab464e7f5116d09281e1e3188e9416e6dfa69
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-04-26 20:53:40 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -76,7 +76,7 @@ async def rename_doc(bot, update):
                 )
                 return
             new_file_name = download_location + file_name
-            os.rename(the_real_download_location, new_file_name)
+           rename(the_real_download_location, new_file_name)
             await bot.edit_message_text(
                 text=Translation.UPLOAD_START,
                 chat_id=update.chat.id,
