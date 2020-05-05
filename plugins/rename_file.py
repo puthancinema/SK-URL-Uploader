955f7c0ec25f1c604d514037aa63369777e956e4
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-05 17:08:10 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -33,12 +33,13 @@ from PIL import Image
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["rename"]))
-async def rename_doc(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
+def rename_doc(bot, update):
+    TRChatBase(update.from_user.id, update.text, "rename")
+    if str(update.from_user.id) not in Config.AUTH_USERS:
+        bot.send_message(
             chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
+            text=Translation.NOT_AUTH_USER_TEXT,
+            reply_to_message_id=update.message_id
         )
         return
     TRChatBase(update.from_user.id, update.text, "rename")
@@ -63,13 +64,13 @@ async def rename_doc(bot, update):
             )
         )
         if the_real_download_location is not None:
-             bot.edit_message_text(
+            await bot.edit_message_text(
                 text=Translation.SAVED_RECVD_DOC_FILE,
                 chat_id=update.chat.id,
                 message_id=a.message_id
             )
             if "IndianMovie" in the_real_download_location:
-                 bot.edit_message_text(
+                await bot.edit_message_text(
                     text=Translation.RENAME_403_ERR,
                     chat_id=update.chat.id,
                     message_id=a.message_id
