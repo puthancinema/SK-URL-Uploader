0c46679bd05ae37500dc39b83dc72d15d2600607
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-05 17:16:25 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -62,7 +62,19 @@ async def rename_doc(bot, update):
                 c_time
             )
         )
-            return
+        if the_real_download_location is not None:
+            await bot.edit_message_text(
+                text=Translation.SAVED_RECVD_DOC_FILE,
+                chat_id=update.chat.id,
+                message_id=a.message_id
+            )
+            if "IndianMovie" in the_real_download_location:
+                await bot.edit_message_text(
+                    text=Translation.RENAME_403_ERR,
+                    chat_id=update.chat.id,
+                    message_id=a.message_id
+                )
+                return
             new_file_name = download_location + file_name
             os.rename(the_real_download_location, new_file_name)
             await bot.edit_message_text(
