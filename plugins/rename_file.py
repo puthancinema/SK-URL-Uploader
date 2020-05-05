c8ad1f91abb367d67210f0805a33633560613e5a
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-05 16:58:53 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -63,13 +63,13 @@ async def rename_doc(bot, update):
             )
         )
         if the_real_download_location is not None:
-            await bot.edit_message_text(
+             bot.edit_message_text(
                 text=Translation.SAVED_RECVD_DOC_FILE,
                 chat_id=update.chat.id,
                 message_id=a.message_id
             )
             if "IndianMovie" in the_real_download_location:
-                await bot.edit_message_text(
+                 bot.edit_message_text(
                     text=Translation.RENAME_403_ERR,
                     chat_id=update.chat.id,
                     message_id=a.message_id
