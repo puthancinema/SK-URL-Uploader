fc3bc786a7c871c8f7b5006b44f1f8deef54d635
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-05 16:34:57 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -10,25 +10,37 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
+    # reg: Procedures
+    UTUBE_BOT_USERS =  set(str(x) for x in os.environ.get("UTUBE_BOT_USERS", "").split())
+    SUPER_DLBOT_USERS =  set(str(x) for x in os.environ.get("SUPER_DLBOT_USERS", "").split())
+    SUPER3X_DLBOT_USERS =  set(str(x) for x in os.environ.get("SUPER3X_DLBOT_USERS", "").split())
+    SUPER7X_DLBOT_USERS =  set(str(x) for x in os.environ.get("SUPER7X_DLBOT_USERS", "").split())
+    BANNED_USERS =  set(str(x) for x in os.environ.get("BANNED_USERS", "").split())
+    # Wat was I thinking? :\
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
-    MAX_FILE_SIZE = 1572864000
+    MAX_FILE_SIZE = 50000000
     TG_MAX_FILE_SIZE = 1572864000
-    FREE_USER_MAX_FILE_SIZE = 1572864000
+    FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    # for storing the user details
+    DB_SQLALCHEMY = "USERS.session"
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
     # https://t.me/hevcbay/951
-    OUO_IO_API_KEY = ""
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
+    OUO_IO_API_KEY = "lcJyZwbl"
+    # for Google Custom Search Engine
+    GCS_API_KEY = os.environ.get("GCS_API_KEY", None)
+    GCS_SE_ID = os.environ.get("GCS_SE_ID", None)
+    # dict to hold the ReQuest queue
+    ADL_BOT_RQ = {}
     # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 0
-    # watermark file
-    DEF_WATER_MARK_FILE = ""
+    PROCESS_MAX_TIMEOUT = 3600
+    # dict to hold Google Drive SignIns
+    G_DRIVE_AUTH_DRQ = {}
