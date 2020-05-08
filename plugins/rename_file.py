0015c4615c22409c6d76c6576d891804dd5b2acb
amalpm615 <52080988+amalpm615@users.noreply.github.com>
2020-05-08 13:03:35 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -33,7 +33,7 @@ from PIL import Image
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["rename"]))
-async def rename_doc(bot, update):
+def rename_doc(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
